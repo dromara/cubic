9f46e6605204dd954061f3f6bdc4bb6fdf5123ef
QIANGLU <598505651@qq.com>
2020-06-05 22:44:34 +0800

    优化打包脚本，增加UI 动态加载

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 cd "${0%/*}"
 cd ..
+BASE_DIR=`pwd`
+echo $BASE_DIR
+BASE_UI_DIR="cubic-ui"
 
 mvn -v
 if [ $? -ne 0 ]; then
@@ -10,7 +13,12 @@ if [ $? -ne 0 ]; then
 fi
 
 #打包agent
+echo "================ starting to build cubic ui ================"
+cd "$BASE_UI_DIR"
+echo `pwd`
+npm run build:prod
 echo "================ starting to build cubic agent ================"
+cd ..
 ./mvnw clean package  -DskipTests
 echo "================ building cubic agent finished ================"
 
