55913e9b288fed1fc797169eb28052f4fa63268c
QIANGLU <598505651@qq.com>
2020-12-09 19:14:56 +0800

    优化thread

--- cubic-agent/src/main/java/com/cubic/agent/jvm/thread/JVMThreadService.java
+++ cubic-agent/src/main/java/com/cubic/agent/jvm/thread/JVMThreadService.java
@@ -4,16 +4,22 @@ import com.cubic.agent.boot.CommonService;
 import com.cubic.agent.boot.DefaultNamedThreadFactory;
 import com.cubic.agent.boot.DefaultService;
 import com.cubic.agent.boot.ServiceManager;
+import com.cubic.agent.conf.AgentConfig;
 import com.cubic.agent.conf.RemoteDownstreamConfig;
 import com.cubic.agent.dictionary.DictionaryUtil;
 import com.cubic.agent.module.ThreadMetricCollection;
 import com.cubic.agent.remote.*;
 import com.cubic.proxy.common.thread.RunnableWithExceptionProtection;
+import com.google.gson.Gson;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.util.concurrent.DefaultThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,69 +31,77 @@ import java.util.concurrent.TimeUnit;
 @DefaultService
 public class JVMThreadService implements CommonService {
 
-	private static final Logger logger = LoggerFactory.getLogger(JVMThreadService.class);
+    private static final Logger logger = LoggerFactory.getLogger(JVMThreadService.class);
 
-	private volatile ScheduledFuture<?> sendMetricFuture;
-	private Sender sender;
+    private volatile ScheduledFuture<?> sendMetricFuture;
+    private Sender sender;
+    private volatile AgentNettyClient client;
 
-	@Override
-	public void prepare() {
-		sender = new Sender();
-		ServiceManager.INSTANCE.findService(AgentClientManager.class).addListener(sender);
-	}
+    @Override
+    public void prepare() {
+        sender = new Sender();
+        ServiceManager.INSTANCE.findService(AgentClientManager.class).addListener(sender);
+    }
 
-	@Override
-	public void start() {
-		sendMetricFuture = Executors
-				.newSingleThreadScheduledExecutor(new DefaultNamedThreadFactory("JVMService-consume"))
-				.scheduleAtFixedRate(new RunnableWithExceptionProtection(sender, th -> {
-					logger.error("JVMService consumes and upload failure.", th);
-				}), 0, 1, TimeUnit.MINUTES);
-	}
+    @Override
+    public void start() {
 
-	@Override
-	public void shutdown() {
-		sendMetricFuture.cancel(true);
-	}
+        sendMetricFuture = new ScheduledThreadPoolExecutor(1, new DefaultThreadFactory("JVMService-consume")).scheduleAtFixedRate(new RunnableWithExceptionProtection(sender, th -> {
+            logger.error("JVMService consumes and upload failure.", th);
+        }), 0, 1, TimeUnit.MINUTES);
 
-	@Override
-	public void complete() {
+    }
 
-	}
+    @Override
+    public void shutdown() {
+        sendMetricFuture.cancel(true);
+    }
 
-	class Sender implements Runnable, AgentChannelListener {
-		private volatile ChannelStatus status = ChannelStatus.DISCONNECT;
+    @Override
+    public void complete() {
 
-		@Override
-		public void run() {
-			if (RemoteDownstreamConfig.Agent.SERVICE_ID != DictionaryUtil.nullValue()
-				&& RemoteDownstreamConfig.Agent.SERVICE_INSTANCE_ID != DictionaryUtil.nullValue()) {
-				if (status == ChannelStatus.CONNECTION) {
-					try {
-						long currentTimeMillis = System.currentTimeMillis();
-						ThreadMetricCollection.Builder builder = new ThreadMetricCollection.Builder();
-						builder.setThreadDump(ThreadProvider.INSTANCE.getThreadDump());
-						builder.setAllThreadPools(ThreadPoolProvider.INSTANCE.getDubboThreadPool());
-						builder.setServiceInstanceId(RemoteDownstreamConfig.Agent.SERVICE_INSTANCE_ID);
-						builder.setTime(currentTimeMillis);
-						builder.setInstanceUUID(ServiceAndEndpointRegisterClient.getInstanceUuid());
-						builder.build();
+    }
 
-					} catch (Throwable t) {
-						logger.error("send JVM thread metrics to Collector fail.", t);
-					}
-				}
-			}
-		}
+    class Sender implements Runnable, AgentChannelListener {
+        private volatile ChannelStatus status = ChannelStatus.DISCONNECT;
 
-		@Override
-		public void statusChanged(ChannelStatus status) {
-			if (ChannelStatus.CONNECTION.equals(status)) {
-//				Channel channel = ServiceManager.INSTANCE.findService(GRPCChannelManager.class).getChannel();
-//				stub = ThreadMetricReportServiceGrpc.newBlockingStub(channel);
-				logger.info("jvm info connection.");
-			}
-			this.status = status;
-		}
-	}
+        @Override
+        public void run() {
+
+            if (status == ChannelStatus.CONNECTION) {
+                try {
+                    long currentTimeMillis = System.currentTimeMillis();
+                    ThreadMetricCollection.Builder builder = new ThreadMetricCollection.Builder();
+                    builder.setThreadDump(ThreadProvider.INSTANCE.getThreadDump());
+                    builder.setAllThreadPools(ThreadPoolProvider.INSTANCE.getDubboThreadPool());
+                    builder.setServiceName(AgentConfig.Agent.SERVICE_NAME);
+                    builder.setTime(currentTimeMillis);
+                    builder.setInstanceUUID(AgentConfig.Agent.INSTANCE_UUID);
+
+                    Gson gson = new Gson();
+                    client.getChannel().writeAndFlush(gson.toJson(builder.build())).addListener(new ChannelFutureListener() {
+                        @Override
+                        public void operationComplete(ChannelFuture future) throws Exception {
+                            if (!future.isSuccess()) {
+                                logger.error("JVMThreadService send {} fail");
+                            } else {
+                                logger.debug("JVMThreadService :{}  channel : {} ");
+                            }
+                        }
+                    });
+                } catch (Throwable t) {
+                    logger.error("send JVM thread metrics to Collector fail.", t);
+                }
+            }
+        }
+
+        @Override
+        public void statusChanged(ChannelStatus status) {
+            if (ChannelStatus.CONNECTION.equals(status)) {
+                client = ServiceManager.INSTANCE.findService(AgentClientManager.class).getClient();
+
+            }
+            this.status = status;
+        }
+    }
 }
--- cubic-agent/src/main/java/com/cubic/agent/module/ThreadMetricCollection.java
+++ cubic-agent/src/main/java/com/cubic/agent/module/ThreadMetricCollection.java
@@ -7,75 +7,75 @@ package com.cubic.agent.module;
  **/
 public class ThreadMetricCollection {
 
-	public static Builder newBuilder() {
-		return new Builder();
-	}
+    public static Builder newBuilder() {
+        return new Builder();
+    }
 
-	private Builder builder;
+    private Builder builder;
 
-	public Builder getBuilder() {
-		return builder;
-	}
+    public Builder getBuilder() {
+        return builder;
+    }
 
-	public static class Builder {
+    public static class Builder {
 
-		private String threadDump;
-		private String allThreadPools;
-		private int serviceInstanceId;
-		private long time;
-		private String instanceUUID;
+        private String threadDump;
+        private String allThreadPools;
+        private String serviceName;
+        private long time;
+        private String instanceUUID;
 
-		public String getThreadDump() {
-			return threadDump;
-		}
+        public String getThreadDump() {
+            return threadDump;
+        }
 
-		public String getAllThreadPools() {
-			return allThreadPools;
-		}
+        public String getAllThreadPools() {
+            return allThreadPools;
+        }
 
-		public int getServiceInstanceId() {
-			return serviceInstanceId;
-		}
+        public String getServiceName() {
+            return serviceName;
+        }
 
-		public long getTime() {
-			return time;
-		}
+        public long getTime() {
+            return time;
+        }
 
-		public String getInstanceUUID() {
-			return instanceUUID;
-		}
+        public String getInstanceUUID() {
+            return instanceUUID;
+        }
 
-		public Builder setThreadDump(String threadDump) {
-			this.threadDump = threadDump;
-			return this;
-		}
+        public Builder setThreadDump(String threadDump) {
+            this.threadDump = threadDump;
+            return this;
+        }
 
-		public Builder setAllThreadPools(String allThreadPools) {
-			this.allThreadPools = allThreadPools;
-			return this;
-		}
+        public Builder setAllThreadPools(String allThreadPools) {
+            this.allThreadPools = allThreadPools;
+            return this;
+        }
 
-		public Builder setServiceInstanceId(int serviceInstanceId) {
-			this.serviceInstanceId = serviceInstanceId;
-			return this;
-		}
+        public Builder setServiceName(String serviceName) {
+            this.serviceName = serviceName;
+            return this;
+        }
 
-		public Builder setTime(long time) {
-			this.time = time;
-			return this;
-		}
+        public Builder setTime(long time) {
+            this.time = time;
+            return this;
+        }
 
-		public Builder setInstanceUUID(String instanceUUID) {
-			this.instanceUUID = instanceUUID;
-			return this;
-		}
+        public Builder setInstanceUUID(String instanceUUID) {
+            this.instanceUUID = instanceUUID;
+            return this;
+        }
 
-		public ThreadMetricCollection build() {
-			return new ThreadMetricCollection(this);
-		}
-	}
+        public ThreadMetricCollection build() {
+            return new ThreadMetricCollection(this);
+        }
+    }
 
-	public ThreadMetricCollection(Builder builder) {
-		this.builder = builder;
-	}
+    public ThreadMetricCollection(Builder builder) {
+        this.builder = builder;
+    }
 }
