69783384b052149d25a4b5f0551cac5c69346a57
haozi <haozi2015@aliyun.com>
2021-04-07 16:46:52 +0800

    添加线程池监控数据收集

---
+++ cubic-proxy/src/main/java/com/matrix/proxy/mapper/ThreadPoolMapper.java
@@ -0,0 +1,13 @@
+package com.matrix.proxy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.matrix.proxy.entity.ThreadPoolEntity;
+
+/**
+ * 线程池持久化
+ *
+ * @author zhanghao
+ * @date 2021/4/74:24 下午
+ */
+public interface ThreadPoolMapper extends BaseMapper<ThreadPoolEntity> {
+}
---
+++ cubic-proxy/src/main/java/com/matrix/proxy/server/process/JvmThreadPoolProcess.java
@@ -0,0 +1,58 @@
+package com.matrix.proxy.server.process;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.cubic.proxy.common.constant.CommandCode;
+import com.cubic.proxy.common.module.Message;
+import com.matrix.proxy.entity.ThreadPoolEntity;
+import com.matrix.proxy.mapper.ThreadPoolMapper;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * 处理线程池信息
+ *
+ * @author zhanghao
+ * @date 2021/4/611:49 上午
+ */
+@Service
+public class JvmThreadPoolProcess extends DefaultMessageProcess {
+    private static final Logger logger = LoggerFactory.getLogger(JvmThreadPoolProcess.class);
+
+    @Resource
+    private ThreadPoolMapper threadPoolMapper;
+
+    @Override
+    public Integer code() {
+        return CommandCode.JVM_THREAD_POOL.getCode();
+    }
+
+    @Override
+    public void process(ChannelHandlerContext ctx, String datagram) {
+        super.process(ctx, datagram);
+        logger.info("receive datagram" + datagram);
+        insertThreadPool(JSON.parseObject(datagram, Message.class));
+    }
+
+    private void insertThreadPool(Message message) {
+        if (message == null) {
+            return;
+        }
+        JSONObject body = JSON.parseObject(message.getBody());
+        Date data = new Date();
+        body.forEach((key, params) -> {
+            threadPoolMapper.insert(ThreadPoolEntity.builder()
+                    .instanceId(message.getInstanceUuid())
+                    .instanceName(message.getInstanceName())
+                    .threadPoolKey(key)
+                    .threadPoolParams(params.toString())
+                    .createTime(data)
+                    .build());
+        });
+    }
+}
