67b449337610ca6e5bfc3d507d7628b8ae3af423
QIANGLU <598505651@qq.com>
2021-04-08 23:32:30 +0800

    1、移除pagehelper 2、优化jvmdump的抓取

--- cubic-proxy/src/main/java/com/matrix/proxy/controller/ThreadMonitorController.java
+++ cubic-proxy/src/main/java/com/matrix/proxy/controller/ThreadMonitorController.java
@@ -1,15 +1,10 @@
 package com.matrix.proxy.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.cubic.proxy.common.module.Response;
-import com.matrix.proxy.constant.MonitorCacheType;
-import com.matrix.proxy.module.ChartModel;
-import com.matrix.proxy.module.ThreadPoolLog;
-import com.matrix.proxy.service.ThreadMonitorService;
+import com.matrix.proxy.service.ThreadDumpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -33,122 +28,22 @@ import java.util.Map;
 @CrossOrigin(maxAge = 3600)
 public class ThreadMonitorController {
 
-	private static Logger log = LoggerFactory.getLogger(ThreadMonitorController.class);
+    private static Logger log = LoggerFactory.getLogger(ThreadMonitorController.class);
 
-	@Resource
-	private ThreadMonitorService threadMonitorService;
+    @Resource
+    private ThreadDumpService threadDumpService;
 
-	@RequestMapping("/getThreadCharts")
-	public String getThreadCharts(@RequestParam String serviceName, @RequestParam String uuid, @RequestParam(defaultValue = "30", required = false) Integer timePeriod, @RequestParam(required = false) String endTime) {
-		Response<List<?>> response = new Response<>(0, "SUCCESS");
-		List<ChartModel> models = threadMonitorService.chartData(uuid, endTime, timePeriod, serviceName);
-		response.setData(models);
-		return JSONObject.toJSONString(response);
-	}
 
-	@RequestMapping("/getThreadDetails")
-	public Response<List> getThreadDetails(@RequestParam String uid, @RequestParam String serviceName, @RequestParam(defaultValue = "30", required = false) Integer timePeriod, @RequestParam(required = false) String endTime) {
-		Response<List> response = new Response<>(0, "SUCCESS");
-		List models = threadMonitorService.getThreadDetails(endTime, timePeriod, serviceName, uid);
-		response.setData(models);
-		return response;
+    /**
+     * 获取某一分钟线程详情
+     *
+     * @param appId
+     * @param dayTime     yyyyMmddHHmmss
+     * @return
+     */
+    @RequestMapping(value = "/getThreadDumpByAppId")
+    public String getThreadsDetailsByMin(@RequestParam String appId,   @RequestParam(required = false) String dayTime ) {
+        return  threadDumpService.getThreadDumpByAppId(appId,dayTime);
 
-	}
-
-	/**
-	 * 获取应用实例所有线程详情
-	 *
-	 * @return
-	 */
-	@RequestMapping("/getThreadsDetailsByUid")
-	public String getThreadsDetailsByUid(@RequestParam String uid, @RequestParam String serviceName, @RequestParam String dayTime) {
-		Response<String> response = new Response<>(0, "SUCCESS");
-		String models = threadMonitorService.getThreadsDetailsByUid(dayTime, serviceName, uid);
-		response.setData(models);
-		return JSONObject.toJSONString(response);
-
-	}
-
-	/**
-	 * 获取应用实例所有线程详情
-	 *
-	 * @return
-	 */
-	@RequestMapping(value = "/getThreadsDetails")
-	public ResponseEntity<String> getThreadsDetails(@RequestParam String uid, @RequestParam String serviceName, @RequestParam String dayTime) {
-		String models = threadMonitorService.getThreadsDetailsByUid(dayTime, serviceName, uid);
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "text/plain;charset=UTF-8");
-		return ResponseEntity.status(200).headers(headers).body(models);
-
-	}
-
-	/**
-	 * 获取实时应用线程栈
-	 *
-	 * @return
-	 */
-	@RequestMapping(value = "/getRealThreadsDetails")
-	public String getRealThreadsDetails(@RequestParam String uid, @RequestParam String serviceName) {
-		Response<String> response = new Response<>(0, "SUCCESS");
-		String threadDump = threadMonitorService.getRealThreadsDetails(serviceName, uid);
-		response.setData(threadDump);
-		return JSONObject.toJSONString(response);
-
-	}
-
-	/**
-	 * 获取某一分钟线程详情
-	 *
-	 * @param uid
-	 * @param serviceName
-	 * @param dayTime     yyyyMmddHHmmss
-	 * @return
-	 */
-	@RequestMapping(value = "/getThreadsDetailsByMin")
-	public String getThreadsDetailsByMin(@RequestParam String uid, @RequestParam String serviceName, @RequestParam String dayTime, @RequestParam(required = false, defaultValue = "RUNNABLE") String threadState) {
-		Response<Map> response = new Response<>(0, "SUCCESS");
-		Map<String, Object> logs = threadMonitorService.getThreadDetails(dayTime, serviceName, uid, threadState);
-		response.setData(logs);
-		return JSONObject.toJSONString(response);
-
-	}
-
-	/**
-	 * 获取应用所有线程信息
-	 *
-	 * @return
-	 */
-	@RequestMapping("/getEndpoints")
-	@Deprecated
-	public Map<Object, Object> getEndpoints(@RequestParam String serviceId, @RequestParam String uuid, @RequestParam String dayTime) {
-		String key = serviceId + ":" + uuid + ":" + dayTime;
-		return MatrixRedisClusterUtils.opsForHash().entries(MonitorCacheType.MATRIX_MONITOR_JVM_THREAD_IDS + key);
-
-	}
-
-	/**
-	 * 获取应用所有线程信息
-	 *
-	 * @return
-	 */
-	@RequestMapping("/getThreads")
-	@Deprecated
-	public Map<Object, Object> getThreads(@RequestParam String serviceId, @RequestParam String dayTime) {
-		String key = serviceId + ":" + dayTime;
-		return MatrixRedisClusterUtils.opsForHash().entries(MonitorCacheType.MATRIX_MONITOR_JVM_THREADS + key);
-
-	}
-
-	@RequestMapping("/getThreadPoolByUid")
-	public String getThreadPoolByUid(@RequestParam String uid, @RequestParam(defaultValue = "0") int pageIndex, @RequestParam(defaultValue = "10") int pageSize, @RequestParam(required = false, defaultValue = "30") String timePeriod, @RequestParam(required = false) String endDate) {
-		Response<ExwarnPage> response = new Response<>(0, "SUCCESS");
-		Page<ThreadPoolLog> exwarnPage = threadMonitorService.getThreadPoolByUid(uid, pageIndex != 0 ? pageIndex - 1 : pageIndex, pageSize, timePeriod, endDate);
-		ExwarnPage<List> listExwarnPage = new ExwarnPage<>();
-		listExwarnPage.setPageIndex(pageIndex);
-		listExwarnPage.setTotal(exwarnPage.getTotalElements());
-		listExwarnPage.setRecords(exwarnPage.getContent());
-		response.setData(listExwarnPage);
-		return JSON.toJSONString(response);
-	}
+    }
 }
---
+++ cubic-proxy/src/main/java/com/matrix/proxy/service/ThreadDumpServiceImpl.java
@@ -0,0 +1,96 @@
+package com.matrix.proxy.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jayway.jsonpath.Criteria;
+import com.matrix.proxy.entity.ThreadDump;
+import com.matrix.proxy.mapper.ThreadDumpMapper;
+import com.matrix.proxy.module.ThreadPoolLog;
+import com.matrix.proxy.module.ThreadStackLog;
+import com.matrix.proxy.util.DateUtils;
+import com.matrix.proxy.util.GzipUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * 处理线程栈信息展示
+ *
+ * @Author qinqixuan
+ * @Date 2021/04/06
+ * @Version V1.0
+ **/
+@Service
+public class ThreadDumpServiceImpl implements ThreadDumpService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThreadDumpServiceImpl.class);
+
+    @Resource
+    private ThreadDumpMapper threadDumpMapper;
+
+
+    /**
+     * 获取应用实例线程栈信息
+     *
+     * @param time
+     * @param appId
+     * @return
+     */
+    @Override
+    public String getThreadDumpByAppId(String time, String appId) {
+//        LocalDateTime localDate = StringUtils.contains(time, "-") ? DateUtils.getTimeByPattern(time, "yyyy-MM-dd HH:mm") : DateUtils.getTimeByPattern(time, "yyyyMMddHHmm");
+//		String minTime = localDate.format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+        QueryWrapper<ThreadDump> wrapper = new QueryWrapper<>();
+        wrapper.eq("appId", appId);
+        ThreadDump threadDump = threadDumpMapper.selectOne(wrapper);
+        return threadDump == null ? "" : GzipUtils.decompress(threadDump.getThreadDump());
+    }
+
+//	public ThreadStackLog getDetails(String serviceId, String uid, String date) {
+//		Criteria criteria = Criteria.where("serviceId").is(serviceId).and("uuid").is(uid).and("timeBucket").is(date);
+//		Query query = new Query(criteria);
+//		ThreadStackLog log = mongoTemplate.findOne(query, ThreadStackLog.class, MongoConstant.THREAD_STACK);
+//		return log;
+//	}
+
+//
+//    /**
+//     * 根据 uid查询 threadpool
+//     *
+//     * @param uid
+//     * @param pageIndex
+//     * @param pageSize
+//     * @param startDate
+//     * @param endDate
+//     * @return
+//     */
+//    @Override
+//    public Page<ThreadPoolLog> getThreadPoolByUid(String uid, int pageIndex, int pageSize, String startDate, String endDate) {
+//        Sort sort = Sort.by(Sort.Direction.DESC, "createDate");
+//        Pageable pageable = PageRequest.of(pageIndex, pageSize, sort);
+//        Criteria criteria = Criteria.where("uuid").is(uid);
+//        if (StringUtils.isNotEmpty(startDate)) {
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//            LocalDateTime end = DateUtils.getTime(endDate);
+//            LocalDateTime start = StringUtils.contains(startDate, "-") ? DateUtils.str2Date(startDate, formatter) : end.minusMinutes(Long.valueOf(startDate));
+////			criteria.andOperator(Criteria.where("createDate").lte(end), Criteria.where("createDate").gte(start));
+//        }
+////		Query query = new Query(criteria);
+////		long count = mongoTemplate.count(query, ThreadPoolLog.class, MongoConstant.THREAD_POOLS);
+////		query.with(pageable);
+////		List<ThreadPoolLog> logs = mongoTemplate.find(query, ThreadPoolLog.class, MongoConstant.THREAD_POOLS);
+//        List<String> result = new LinkedList<>();
+////		logs.forEach(log -> {
+////			result.add(log.getDetails());
+////		});
+////		return new PageImpl(result, pageable, count);
+//        return null;
+//    }
+}
