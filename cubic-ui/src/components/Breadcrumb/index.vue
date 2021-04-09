0f97818526f8db84792ebe4131e1784076e75539
QIANGLU <598505651@qq.com>
2021-04-09 22:18:57 +0800

    优化页面布局

--- cubic-ui/src/components/Breadcrumb/index.vue
+++ cubic-ui/src/components/Breadcrumb/index.vue
@@ -34,7 +34,7 @@ export default {
       const first = matched[0]
 
       if (!this.isDashboard(first)) {
-        matched = [{ path: '/dashboard', meta: { title: 'Dashboard' }}].concat(matched)
+        matched = [{ path: '/dashboard', meta: { title: '应用中心' }}].concat(matched)
       }
 
       this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
