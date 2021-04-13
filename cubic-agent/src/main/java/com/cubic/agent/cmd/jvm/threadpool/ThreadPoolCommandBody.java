7e642791c1dd7bc9d94714986a2d269dd121841e
haozi <haozi2015@aliyun.com>
2021-04-13 17:10:01 +0800

    线程池指令下发agent

---
+++ cubic-agent/src/main/java/com/cubic/agent/cmd/jvm/threadpool/ThreadPoolCommandBody.java
@@ -0,0 +1,38 @@
+package com.cubic.agent.cmd.jvm.threadpool;
+
+/**
+ * 线程池下发命令接收对象
+ *
+ * @author zhanghao
+ * @date 2021/4/134:58 下午
+ */
+public class ThreadPoolCommandBody {
+    private String key;
+    private String name;
+    private Object arg;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getArg() {
+        return arg;
+    }
+
+    public void setArg(Object arg) {
+        this.arg = arg;
+    }
+
+}
